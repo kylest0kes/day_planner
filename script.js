68bc24acd909b24b2715e559297feef456e71e47
kylest0kes <kylestokes1551@gmail.com>
2020-04-20 22:33:13 -0400

    minor style and js tweaks

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var timeAndDay = $("#currentDay");
 
-//console.log(currentDayAndTime)
-
 setInterval(function() {
     var currentDayAndTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     timeAndDay.text(currentDayAndTime)
-    console.log(currentDayAndTime)
+    //console.log(currentDayAndTime)
 }, 1000);
 
 
