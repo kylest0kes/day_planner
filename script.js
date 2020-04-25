29766b109b458b074581c176d9835c9953a4f2b4
kylest0kes <kylestokes1551@gmail.com>
2020-04-25 13:38:35 -0400

    got the css styles linked to current time and adjust accordingly

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 var timeAndDay = $("#currentDay");
 var rowElements = $(".row")
-var currentDayAndTime;
+var currentDate = moment().format('MMMM Do YYYY');
+var timeBlock = $('.hour');
 
+var currentDayAndTime = moment().format('dddd | MMMM Do YYYY | h:mm:ss a');
+    timeAndDay.text(currentDayAndTime)
 
-
-//getting current day and time
 setInterval(function() {
-    currentDayAndTime = moment().format('dddd | MMMM Do YYYY | h:mm a');
+    var currentDayAndTime = moment().format('dddd | MMMM Do YYYY | h:mm:ss a');
     timeAndDay.text(currentDayAndTime)
 }, 1000);
 
-//changing styles based on the current hour (isSame, isBefore, isAfter)
+//function to get the value of the row
+function getValue() {
+    for(var i = 0; i < rowElements.length; i++) {
+        var timeBlockValue = parseInt($(timeBlock[i]).attr('data-hour'));
+        var currentRow = $(rowElements[i])
+        var hourValue = moment().hours();
+        if (timeBlockValue < hourValue){
+            currentRow.addClass('past')
+        } else if (timeBlockValue === hourValue){
+            currentRow.removeClass('past')
+            currentRow.addClass('present')
+        } else {
+            currentRow.removeClass('past')
+            currentRow.removeClass('present')
+            currentRow.addClass('future')
+        }     
+    }
+}
+
+getValue()
 
 
 
-$(document).ready(function(){
-    //function for on click of save button to save everything to localstorage
-    $(".saveBtn").on("click", function() {
 
-    });
-    
-})
-
-// Use moment to get current time (hours) and compare to 8AM-6PM hardcoded
-    // Compare the time you get to the 8AM-6PM
-    // highlight those before the hour grey
-    // highlight current hour red
-    // highlight after hour green
-    // add or remove classes with jQuery
     
 // Saving stuff
     // correlate what you save to the date it was saved
     // on initial load, get today's date, compare with what's in local storage, and save that in localstorage
     // when to wipe the events (when the date in localstorage is different from today's date)
+
+
+$(document).ready(function(){
+
+    //function for on click of save button to save everything to localstorage
+    $(".saveBtn").on("click", function() {
+        $(this).siblings('.hour')
+        //console.log($(this).siblings('.hour').attr('data-hour'))
+        $(this).siblings('textarea').val()
+        console.log($(this).siblings('textarea').val())
+    });
+    
+})
+
